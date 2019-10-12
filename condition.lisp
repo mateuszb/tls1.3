01e509ce56b093e87fb89a8df0643ddc54f19e1b
Mateusz Berezecki <mateuszb@fastmail.fm>
2019-10-12 15:04:57 -0400

    Add condition pretty printer

--- condition.lisp
+++ condition.lisp
@@ -4,3 +4,9 @@
 
 (define-condition alert-arrived (error)
   ((alert :initform nil :initarg :alert)))
+
+(defmethod print-object ((condition alert-arrived) stream)
+  (print-unreadable-object (condition stream :type t)
+    (with-slots (alert) condition
+      (with-slots (level description) alert
+	(format stream "level=~a, description=~a" level description)))))
